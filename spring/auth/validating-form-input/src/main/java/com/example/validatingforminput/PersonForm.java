a34cbfc8748d48ce0261ec62493058939bcc956f
Lionel Beato <lionel.beato@gmail.com>
2021-02-27 15:03:00 -0500

    adds validation homework

---
+++ spring/auth/validating-form-input/src/main/java/com/example/validatingforminput/PersonForm.java
@@ -0,0 +1,42 @@
+package com.example.validatingforminput;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonForm{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
