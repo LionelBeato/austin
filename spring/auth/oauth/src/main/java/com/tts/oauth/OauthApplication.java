49c4aa8078c28ec217d839be073a4a6e6c6df31d
Lionel Beato <lionel.beato@gmail.com>
2021-02-27 16:14:55 -0500

    hides secret in oauth

--- spring/auth/oauth/src/main/java/com/tts/oauth/OauthApplication.java
+++ spring/auth/oauth/src/main/java/com/tts/oauth/OauthApplication.java
@@ -3,6 +3,7 @@ package com.tts.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
