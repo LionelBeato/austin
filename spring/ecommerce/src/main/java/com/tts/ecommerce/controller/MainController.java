aebd659d90b644d282492e9bbd590e6aed7db00f
Lionel Beato <lionel.beato@gmail.com>
2021-03-04 13:33:18 -0500

    adds twitter project and ecommerce project

---
+++ spring/ecommerce/src/main/java/com/tts/ecommerce/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.tts.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getMain() {
+        return "main";
+    }
+
+}
